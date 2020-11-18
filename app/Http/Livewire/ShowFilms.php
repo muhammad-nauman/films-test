d19abf3f190c78f95cb7460301b50c287bbf9960
Muhammad Nauman <naumancs@gmail.com>
2020-11-18 17:32:44 +0500

    added
    add

--- app/Http/Livewire/ShowFilms.php
+++ app/Http/Livewire/ShowFilms.php
@@ -9,13 +9,34 @@ class ShowFilms extends Component
 {
     public $films;
     public $currentFilm = 0;
+    public $hasPrevious = true;
+    public $hasNext = true;
 
     public function mount()
     {
-        $this->films = Film::where('id', '>', $this->currentFilm)->first();
+        $this->films = Film::where('id', '>', $this->currentFilm)->where('status', 1)->orderBy('id', 'asc')->first();
+        $this->currentFilm = $this->films->id;
+        $this->hasNext = Film::where('id', '>', $this->currentFilm)->where('status', 1)->orderBy('id', 'asc')->exists();
+        $this->hasPrevious = Film::where('id', '<', $this->currentFilm)->where('status', 1)->orderBy('id', 'asc')->exists();
     }
     public function render()
     {
         return view('livewire.show-films');
+    }
+
+    public function getNext()
+    {
+        $this->films = Film::where('id', '>', $this->currentFilm)->where('status', 1)->orderBy('id', 'asc')->first();
+        $this->currentFilm = $this->films->id;
+        $this->hasNext = Film::where('id', '>', $this->currentFilm)->where('status', 1)->orderBy('id', 'asc')->exists();
+        $this->hasPrevious = Film::where('id', '<', $this->currentFilm)->where('status', 1)->orderBy('id', 'asc')->exists();
+    }
+
+    public function getPrevious()
+    {
+        $this->films = Film::where('id', '<', $this->currentFilm)->where('status', 1)->orderBy('id', 'asc')->first();
+        $this->currentFilm = $this->films->id;
+        $this->hasNext = Film::where('id', '>', $this->currentFilm)->where('status', 1)->orderBy('id', 'asc')->exists();
+        $this->hasPrevious = Film::where('id', '<', $this->currentFilm)->where('status', 1)->orderBy('id', 'asc')->exists();
     }
 }
--- resources/views/livewire/show-films.blade.php
+++ resources/views/livewire/show-films.blade.php
@@ -1,29 +1,33 @@
-<div class="flex items-center justify-center">
+<div>
+    <div class="flex items-center justify-center">
 
-    <div class="w-full md:w-full lg:w-1/2 max-w-4xl rounded overflow-hidden shadow-lg m-4 flex justify-center">
-        <div class="md:flex-shrink-0">
-            <img class="md:w-56"
-                 src="{{ $films->poster }}"
-                 alt="{{ $films->name }}" />
-        </div>
-        <div class="flex flex-col flex-grow px-8 py-4 bg-gray-200">
-            <h3 class="font-bold text-4xl md:text-2xl lg:text-2xl text-purple-400 movie--title">{{ $films->name }}</h3>
-            <span class="movie--year text-xl lg:text-sm lg:mb-4">{{ $films->released_year }}</span>
-            <div class="flex-grow">
-                <p class="text-xl md:text-base lg:text-base text-indigo-400 leading-snug truncate-overflow">{{ $films->description }}</p>
+        <div class="w-full md:w-full lg:w-1/2 max-w-4xl rounded overflow-hidden shadow-lg m-4 flex justify-center">
+            <div class="md:flex-shrink-0">
+                <img class="md:w-56"
+                     src="{{ $films->poster }}"
+                     alt="{{ $films->name }}" />
             </div>
-            <div class="flex justify-center items-center">
-                <div class="flex items-center mt-2 mb-4">
-                    <svg class="mx-1 w-4 h-4 fill-current text-yellow-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z"/></svg>
+            <div class="flex flex-col flex-grow px-8 py-4 bg-gray-200">
+                <h3 class="font-bold text-4xl md:text-2xl lg:text-2xl text-purple-400 movie--title">{{ $films->name }}</h3>
+                <span class="movie--year text-xl lg:text-sm lg:mb-4">{{ \Carbon\Carbon::parse($films->released_date)->year }}</span>
+                <div class="flex-grow">
+                    <p class="text-xl md:text-base lg:text-base text-indigo-400 leading-snug truncate-overflow">{{ $films->description }}</p>
                 </div>
-            </div>
-            <div class="button-container flex justify-between mb-2">
-                <button class="text-lg lg:text-sm font-bold py-2 px-4 rounded bg-orange-200 text-orange-700">More Info</button>
+                <div class="flex">
+                    <div class="flex items-center mt-2 mb-4">
+                        @foreach(range(1, 5) as $value)
+                            <svg class="mx-1 w-4 h-4 fill-current @if($value <= $films->rating) text-yellow-500 @else text-gray-400 @endif" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z"/></svg>
+                        @endforeach
+                    </div>
+                </div>
+                <div class="button-container flex justify-between mb-2">
+                    <button class="text-lg lg:text-sm font-bold py-2 px-4 rounded bg-orange-200 text-orange-700">More Info</button>
+                </div>
             </div>
         </div>
     </div>
-</div>
-<div class="button-container flex justify-center mb-4">
-    <button class="text-lg font-black py-2 px-10 rounded mx-5 bg-purple-400 text-white"><</button>
-    <button class="text-lg font-black py-2 px-10 mx-5 rounded bg-purple-400 text-white">></button>
+    <div class="button-container flex justify-center mb-4">
+        <button class="text-lg font-black py-2 px-10 rounded mx-5 bg-purple-400 text-white @if(!$hasPrevious) cursor-not-allowed @endif" wire:click="getPrevious" @if(!$hasPrevious) disabled @endif><</button>
+        <button class="text-lg font-black py-2 px-10 mx-5 rounded bg-purple-400 text-white @if(!$hasNext) cursor-not-allowed @endif" wire:click="getNext" @if(!$hasNext) disabled @endif>></button>
+    </div>
 </div>
